6d7d6724ee575e60070d6a3167269239ae902e02
Gabrielly Venancio Montezi dos Anjos <2302044@SALAS.AULAS>
2024-03-21 22:34:18 -0300

    atualização tela cadastro front e back

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,29 +1,41 @@
 const url = 'https://api-go-wash-efc9c9582687.herokuapp.com/api/user';
 
 async function cadastroUsuario() {
-  var name = document.getElementById('name');
-  let resposta = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({
-      name: name.value,
-      email: 'carlos@gmail.com',
-      user_type_id: 1,
-      password: '123456',
-      cpf_cnpj: '62418247406',
-      terms: 1,
-      birthday: '2000-10-12',
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  var name = document.getElementById('name').value;
+  var dataNasc = document.getElementById('dataNasc').value;
+  var cpfCnpj = document.getElementById('cpf_cnpj').value;
+  var email = document.getElementById('email').value;
+  var senha = document.getElementById('senha').value;
+  var confSenha = document.getElementById('confSenha').value;
+  var termo = document.getElementById('user_type').value;
+  
+  if (senha == confSenha){
+      let resposta = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        user_type_id: 1,
+        password: senha,
+        cpf_cnpj: cpfCnpj,
+        terms: termo,
+        birthday: dataNasc,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-  let data = await resposta.json();
-
-  if (data.data.statusCode != 200) {
-    alert(data.data.errors?.cpf_cnpj[0]);
-    return;
+    let data = await resposta.json();
+    console.log('a');
+    if (data.data.statusCode != 200) {
+      alert(data.data.errors?.cpf_cnpj[0]);
+      return;
+    }
+    alert('Cadastro feito com sucesso');
+    window.location.href = 'login.html';
   }
-  alert('Cadastro feito com sucesso');
-  window.location.href = 'login.html';
+  else{
+    alert("Senhas não conferem");
+  }
 }
