ca720e8e63708d7d7ee209c24ce8a9ef54e06c6b
gyovanna lima <gyovannalima01@gmail.com>
2024-03-23 13:07:26 -0300

    atualizando script cadastr.js

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,6 +1,10 @@
 const url = 'https://api-go-wash-efc9c9582687.herokuapp.com/api/user';
 
-async function cadastroUsuario() {
+const btnCadastrar = document.querySelector('.btnCadastrar');
+btnCadastrar.addEventListener('click', cadastroUsuario);
+
+async function cadastroUsuario(e) {
+  e.preventDefault();
   var name = document.getElementById('name').value;
   var dataNasc = document.getElementById('dataNasc').value;
   var cpfCnpj = document.getElementById('cpf_cnpj').value;
@@ -8,34 +12,44 @@ async function cadastroUsuario() {
   var senha = document.getElementById('senha').value;
   var confSenha = document.getElementById('confSenha').value;
   var termo = document.getElementById('user_type').value;
-  
-  if (senha == confSenha){
-      let resposta = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        user_type_id: 1,
-        password: senha,
-        cpf_cnpj: cpfCnpj,
-        terms: termo,
-        birthday: dataNasc,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
 
-    let data = await resposta.json();
-    console.log('a');
-    if (data.data.statusCode != 200) {
-      alert(data.data.errors?.cpf_cnpj[0]);
-      return;
+  if (senha == confSenha) {
+    let resposta;
+    let data;
+    let error;
+    try {
+      resposta = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          user_type_id: 1,
+          password: senha,
+          cpf_cnpj: cpfCnpj,
+          terms: termo,
+          birthday: dataNasc,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      data = await resposta.json();
+
+      // if (data.data.statusCode != 200) {
+      //   alert(data.data.errors?.cpf_cnpj[0]);
+      //   return;
+      // }
+      // alert('Cadastro feito com sucesso');
+      if (!resposta.ok) throw new Error(e);
+    } catch (e) {
+      error = e;
+    } finally {
+      console.log(resposta);
+      console.log(data);
+      console.log(error);
+      // window.location.href = 'login.html';
     }
-    alert('Cadastro feito com sucesso');
-    window.location.href = 'login.html';
-  }
-  else{
-    alert("Senhas não conferem");
+  } else {
+    alert('Senhas não conferem');
   }
 }
